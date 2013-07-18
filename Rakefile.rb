e6c6dd154576638447363783caee8ad023c61286
Jain Waldrip <jkwaldri@indiana.edu>
2013-07-18 10:48:52 -0400

    Add select_config task to Rake
    rake select_config set up to copy .yml config file
    from lib/config/alt/
    to lib/config/default_options.yml

--- Rakefile.rb
+++ Rakefile.rb
@@ -12,14 +12,29 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-libdir = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+loaddir = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift(loaddir) unless $LOAD_PATH.include?(loaddir)
 
 require 'rubygems'
 require 'rspec/core/rake_task'
 require 'bundler/gem_tasks'
+require 'fileutils'
 
 desc "Print the current version number."
 task :version do
   puts OLE_QA::Framework::VERSION
+end
+
+desc "Select a config file from lib/config/alt."
+task :select_config do
+  puts 'Enter filename:  '
+  target_file = STDIN.gets.chomp
+  target_file += '.yml' unless target_file =~ /\.yml$/
+  file_path = "#{loaddir}/lib/config/"
+  if File.exists?(file_path + 'alt/' + target_file)
+    FileUtils.cp "#{file_path}alt/#{target_file}", "#{file_path}default_options.yml"
+    puts "Successfully copied #{file_path}alt/#{target_file}."
+  else
+    puts "File not found: #{file_path}alt/#{target_file}"
+  end
 end
