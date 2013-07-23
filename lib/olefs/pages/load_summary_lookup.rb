a4340b1f88e0699629008119f8c503669b5ebfd5
Jain Waldrip <jkwaldri@indiana.edu>
2013-07-23 17:39:53 -0400

    Create load summary page

---
+++ lib/olefs/pages/load_summary_lookup.rb
@@ -0,0 +1,40 @@
+#  Copyright 2005-2013 The Kuali Foundation
+#
+#  Licensed under the Educational Community License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#    http://www.opensource.org/licenses/ecl2.php
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+module OLE_QA::Framework::OLEFS
+  # An OLE Financial System Load Summary Lookup page.
+  class Load_Summary_Lookup < Lookup
+    def initialize(ole_session)
+      url = ole_session.fs_url + 'portal.do?channelTitle=Load%20Reports&channelUrl=batchlookup.do?methodToCall=start&businessObjectClassName=org.kuali.ole.select.businessobject.OleLoadSumRecords&docFormKey=88888888&returnLocation='
+      url += ole_session.fs_url + 'portal.do&hideReturnLink=true'
+      super(ole_session, url)
+    end
+
+    # Define Load Summary Lookup screen elements.
+    def set_elements
+      super
+      element(:description_field)                   {b.text_field(:id => 'acqLoadDescription')}
+      element(:filename_field)                      {b.text_field(:id => 'fileName')}
+      element(:user_id_field)                       {b.text_field(:id => 'principalId')}
+      element(:load_profile_selector)               {b.select_list(:id => 'profileId')}
+      element(:date_from_field)                     {b.text_field(:name => 'rangeLowerBoundKeyPrefix_loadCreatedDate')}
+      element(:date_to_field)                       {b.text_field(:id => 'loadCreatedDate')}
+    end
+
+    # Define commonly used functions for Load Summary Lookup screen.
+    def set_functions
+
+    end
+  end
+end
