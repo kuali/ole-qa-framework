7c2280c4a597ef9c328c3036742aba53be59ba9d
Jain Waldrip <jkwaldri@iu.edu>
2013-11-05 15:53:52 -0500

    Add URL & wait elements to Batch Process Profile Lookup screen

---
+++ lib/olels/pages/batch_process.rb
@@ -0,0 +1,47 @@
+#  Copyright 2005-2013 The Kuali Foundation
+#
+#  Licensed under the Educational Community License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#    http://www.opensource.org/licenses/ecl2.php
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+module OLE_QA::Framework::OLELS
+  # The Batch Process control screen in the OLE Library System
+  class Batch_Process < OLE_QA::Framework::Page
+    # Define URL in initialize.
+    def initialize(ole_session)
+      url = ole_session.url + 'portal.do?channelTitle=Batch Process&channelUrl='
+      url += ole_session.url + 'ole-kr-krad/oleBatchProcessDefinitionController?viewId=OLEBatchProcessDefinitionView&methodToCall=startBatch&command=initiate&documentClass=org.kuali.ole.batch.document.OLEBatchProcessDefinitionDocument'
+      super(ole_session, url)
+    end
+
+    def set_elements
+      super
+      element(:title)                       {b.h1.span}
+      element(:name_field)                  {b.text_field(:id => 'BatchProcessDefinition-batchProcessName_control')}
+      element(:profile_name_field)          {b.text_field(:id => 'BatchProcessDefinition-batchProcessProfileName_control')}
+      element(:profile_search_icon)         {b.input(:title => 'Search Field')}
+      element(:batch_size_field)            {b.text_field(:id => 'chunkSizeField_control')}
+      element(:email_field)                 {b.text_field(:id => 'emailField_control')}
+      element(:output_format_selector)      {b.select_list(:id => 'outputField_control')}
+      element(:output_file_field)           {b.text_field(:id => 'outputFileField_control')}
+      element(:run_button)                  {b.button(:id => 'runNowButton')}
+      element(:schedule_button)             {b.button(:id => 'scheduleButton')}
+    end
+
+    def wait_for_elements
+      super
+      @wait_on << :title
+      @wait_on << :name_field
+      @wait_on << :profile_name_field
+      @wait_on << :run_button
+    end
+  end
+end
