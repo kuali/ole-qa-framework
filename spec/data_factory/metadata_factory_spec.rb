69de16d7cafae98e79fab20cd4e20215d8e1420f
Jain Waldrip <jkwaldri@iu.edu>
2013-12-16 09:46:35 -0500

    Create base spec for metadata location

---
+++ spec/data_factory/metadata_factory_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require 'spec_helper.rb'
+
+describe 'The Metadata Factory' do
+
+  it 'should create a location code' do
+    location = OLE_QA::Framework::Metadata_Factory.location_code
+    location.should be_a(String)
+    location.should =~ /[A-Z,0-9]+/
+  end
