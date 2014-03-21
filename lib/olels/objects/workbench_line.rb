b10f2520fc24c95e5fa6143bd4ad60076db94b78
Jain Waldrip <jkwaldri@iu.edu>
2014-03-21 12:11:28 -0400

    Add workbench line elements

--- lib/olels/objects/workbench_line.rb
+++ lib/olels/objects/workbench_line.rb
@@ -17,6 +17,32 @@ module OLE_QA::Framework::OLELS
   class Workbench_Line < OLE_QA::Framework::Line_Object
     def set_elements
       super
+      element(:search_field)                {(line_id == 0) ?
+                                              id_str = 'SearchConditions_SearchText_id_add_control' :
+                                              id_str = "SearchConditions_SearchText_id_line#{line_id - 1}_control"
+                                              b.text_field(:id => id_str)}
+      element(:search_scope_selector)       {(line_id == 0) ?
+                                              id_str = 'SearchConditions_Operator_id_add_control' :
+                                              id_str = "SearchConditions_Operator_id_line#{line_id - 1}_control"
+                                              b.select_list(:id => id_str)}
+      element(:field_selector)              {(line_id == 0) ?
+                                              id_str = 'SearchConditions_DocField_id_add_control' :
+                                              id_str = "SearchConditions_DocField_id_line#{line_id - 1}_control"
+                                              b.select_list(:id => id_str)}
+      element(:join_and)                    {(line_id == 0) ?
+                                              id_str = 'SearchConditions_SearchScope_id_add_control_0' :
+                                              id_str = "SearchConditions_SearchScope_id_line#{line_id - 1}_control_0"
+                                              b.radio(:id => id_str)}
+      element(:join_or)                     {(line_id == 0) ?
+                                              id_str = 'SearchConditions_SearchScope_id_add_control_1' :
+                                              id_str = "SearchConditions_SearchScope_id_line#{line_id - 1}_control_1"
+                                              b.radio(:id => id_str)}
+      element(:join_not)                    {(line_id == 0) ?
+                                              id_str = 'SearchConditions_SearchScope_id_add_control_2' :
+                                              id_str = "SearchConditions_SearchScope_id_line#{line_id - 1}_control_2"
+                                              b.radio(:id => id_str)}
+      element(:add_button)                  {b.button(:id => 'SearchConditionsSection_add')}
+      element(:delete_button)               {b.button(:id => "SearchConditionsSection_del_line#{line_id - 1}")}
     end
   end
 end
