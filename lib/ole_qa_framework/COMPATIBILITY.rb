a1c3f5534fefd4536d37182c8564fdf075ec9713
Jain Waldrip <jkwaldri@iu.edu>
2014-03-21 11:35:16 -0400

    Update compatibility version

--- lib/ole_qa_framework/COMPATIBILITY.rb
+++ lib/ole_qa_framework/COMPATIBILITY.rb
@@ -15,6 +15,6 @@
 module OLE_QA
   module Framework
     # The most recent version of OLE with which this framework was tested for compatibility.
-    OLE_VERSION = '1.5.0 - M2 (Rice Upgrade 2.3.2)'
+    OLE_VERSION = '1.5.0 - M2 - r18095, 2014/03/21'
   end
 end
